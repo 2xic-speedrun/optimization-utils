6b220aec2e783b3818e87a52ce17d4fc2389a7b1
2xic <5640782+2xic@users.noreply.github.com>
2022-08-23 21:20:31 +0200

    restructure and readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Optimization utils
+
+Homemade tool for various optimization algorithms, and or helper functions for optimization algorithm (i.e replay buffers for rl)
--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 import setuptools
 
-with open("README.md", "r", encoding = "utf-8") as fh:
-    long_description = fh.read()
+print(setuptools.find_packages(where="src"))
 
 setuptools.setup(
     name = "optimization_utils",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "2xic",
     package_dir = {"": "src"},
     packages = setuptools.find_packages(where="src"),
-    python_requires = ">=3.6"
 )
