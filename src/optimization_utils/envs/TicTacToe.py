7a35932b288af6e81bb849adce19ef0f4d549479
2xic <5640782+2xic@users.noreply.github.com>
2023-01-17 22:24:49 +0100

    Added computer mode to tic tac toe

--- src/optimization_utils/envs/TicTacToe.py
+++ src/optimization_utils/envs/TicTacToe.py
@@ -1,7 +1,15 @@
 import numpy as np
+from copy import deepcopy
+import random
 
 class TicTacToe:
-    def __init__(self, n=3) -> None:
+    def __init__(self, n=3, is_auto_mode=False) -> None:
+        self.is_auto_mode = is_auto_mode
+        self.n = n
+        self.reset()
+
+    def reset(self):
+        n = self.n
         self.state = np.zeros((n, n))
         # player 1 and -1
         self.player = 1
@@ -10,19 +18,35 @@ class TicTacToe:
         self.done = False
         self.action_space = n * n
 
+    def soft_apply(self, action):
+        state = self.state.copy()
+        state = self.apply_action(action, state)
+        return state
+
     def play(self, action):
         if self.done:
             raise Exception("Game is done.")
 
         if action in self.legal_actions:
+            self.state = self.apply_action(action, self.state)
+            self.switch_player()
+
             self.legal_actions.remove(action)
+            self.done = self.winner is not None or len(self.legal_actions) == 0
+
+            if self.is_auto_mode and self.player == -1 and not self.done:
+                self.play(random.sample(self.legal_actions, k=1)[0])
+        else:
+            raise Exception(f"Illegal action by {self.player} by action {action}")
+
+    def apply_action(self, action, state):
+        if action in self.legal_actions:
             x = action % self.n
             y = action // self.n
 
-            self.state[y][x] = self.player
+            state[y][x] = self.player
 
-            self.switch_player()
-            self.done = self.winner is not None
+            return state
         else:
             raise Exception(f"Illegal action by {self.player} by action {action}")
 
@@ -36,6 +60,4 @@ class TicTacToe:
                 return self.state[:, i][0]
             elif abs(sum(self.state[i, :])) == self.n:
                 return self.state[i, :][0]
-
         return None
-
